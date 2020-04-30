0ce30b99f04d1ceaea92a268bc5b351aef2d7bf5
mattiaguerri <mattguerri1@gmail.com>
2020-04-30 16:44:54 +0200

    clean up

--- decorators.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-def do_twice(func):
-    @functools.wraps(func)
-    def wrapper_do_twice(*args, **kwargs):
-        func(*args, **kwargs)
-        return func(*args, **kwargs)
-    return wrapper_do_twice
